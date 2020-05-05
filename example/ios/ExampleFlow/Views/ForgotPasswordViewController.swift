6b45ec62c88dcb802aac4d8d87ea1238934d5803
Brian Howard <bghoward@inmotionsoftware.com>
2020-05-05 14:54:28 -0500

    Simplified the ForgotPasswordViewController by subclassing BaseFlowViewController

--- example/ios/ExampleFlow/Views/ForgotPasswordViewController.swift
+++ example/ios/ExampleFlow/Views/ForgotPasswordViewController.swift
@@ -11,35 +11,16 @@ import UIKit
 import FlowKit
 import PromiseKit
 
-class ForgotPasswordViewController: UIViewController, FlowViewController {
-    typealias Input = String
-    typealias Output = String
+class ForgotPasswordViewController: BaseFlowViewController<String, String> {
 
-    var delegate: ViewControllerDelegate?
     @IBOutlet var email: UITextField!
     @IBOutlet var error: UILabel!
-
-    private var proxy = Promise<Output>.pending()
-
-    func startFlow(context: String) -> Promise<String> {
-        if (!proxy.promise.isPending) {
-            proxy.resolver.reject(FlowError.canceled)
-            proxy = Promise<Output>.pending()
-        }
-        self.error.text = nil
-        return proxy.promise
-    }
-
-    override func willMove(toParent parent: UIViewController?) {
-        self.delegate?.willMove(toParent: parent)
-        super.willMove(toParent: parent)
-    }
 
     @IBAction
     public func onSubmit() {
         if let email = email.text, email.isValidEmail() {
             self.error.text = nil
-            self.proxy.resolver.fulfill(email)
+            self.resolve(email)
         } else {
             self.error.text = "Invalid email"
         }
